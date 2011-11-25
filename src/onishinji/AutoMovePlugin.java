ee50722ea2c77ab0932e2a770fc2e0217470b433
onishinji <onishinji@gmail.com>
2011-11-25 18:27:20 +0100

    first release

---
+++ src/onishinji/AutoMovePlugin.java
@@ -0,0 +1,229 @@
+package onishinji;
+ 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import onishinji.commands.AutoMoveCommand; 
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AutoMovePlugin extends JavaPlugin {
+
+    private Timer t;
+    
+    HashMap<Player, Timer> timerPlayer;
+    
+    HashMap<Player, ArrayList<Location>> registerUser;
+
+    @Override
+    public void onDisable() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onEnable() {
+        // TODO Auto-generated method stub
+        System.out.println(">>> AutoMove is On");
+        
+        this.registerUser = new HashMap<Player, ArrayList<Location>>();
+        this.timerPlayer = new HashMap<Player, Timer>();
+
+        getCommand("am").setExecutor(new  AutoMoveCommand(this, true)); 
+        t = new Timer();
+
+    }
+
+    public void startTP(final Player player, String[] args) {
+        // TODO Auto-generated method stub
+         
+        Location oldLocation = player.getLocation();
+        final Location newLocation = oldLocation;
+        
+        final int i = 0;
+        
+        
+        t.schedule(new TimerTask() { 
+
+            public void run() {
+                
+                // mouvement sur X
+                newLocation.setX(newLocation.getX() + 0.1);
+                System.out.println("Yaw" +newLocation.getYaw());
+                System.out.println("Pitch " + newLocation.getPitch());
+                
+                // mouvement de tete vers la droite
+                // valeur possible: 
+                // -90(sud)
+                // -180 (est)
+                // 180 (ouest)
+                // 90(orienté nord)
+                newLocation.setYaw((float) (newLocation.getYaw()+0.1));
+                 
+                // mouvement de tete vers le bas, valeur compris entre -90 (vers le haut du ciel)
+                // et +90 tu mattes tes pieds
+                newLocation.setPitch((float) (newLocation.getPitch()+0.1));
+                player.teleport(newLocation);
+                
+            }
+
+        }, 0, 1000);
+        
+    }
+
+    public void stopTP(Player player, String[] args) {
+        t.cancel();    
+        t = new Timer();    
+    }
+    
+    public ArrayList<Location> getLocations(Player player)
+    {
+        if(!(this.registerUser.get(player) != null))
+        {
+            this.registerUser.put(player, new ArrayList<Location>());
+        } 
+
+        return this.registerUser.get(player); 
+    }
+    
+    public Timer getTimer(Player player, boolean b)
+    {
+        if(!(this.timerPlayer.get(player) != null))
+        {
+            this.timerPlayer.put(player, new Timer());    
+        }
+        
+        Timer t = this.timerPlayer.get(player);
+        
+        if(b)
+        {
+            t = new Timer();
+            this.timerPlayer.put(player, t);
+        }
+        
+        return this.timerPlayer.get(player);
+    }
+    
+    public boolean playerCanBeAnimated(Player player)
+    {
+        return this.getLocations(player).size() == 2;
+    }
+
+    public void registerFirstCoordinate(Player player) { 
+            this.getLocations(player).add(0, player.getLocation());
+            player.sendMessage(ChatColor.GREEN + "Tu viens de définir le premier point du déplacement");
+    }
+
+    public void registerSecondCoordinate(Player player) {
+        this.getLocations(player).add(1, player.getLocation());        
+        player.sendMessage(ChatColor.GREEN + "Tu viens de définir le second point du déplacement");
+    }
+
+    public void startLineAnimation(Player player, String intervalTp, String dureTotale) {
+        // TODO Auto-generated method stub
+        
+        if(!this.playerCanBeAnimated(player))
+        {
+            player.sendMessage("Commence par un /am start  et fini par un /am stop");
+        }
+        
+        final long intervalDeTempsEntreLesTp = Long.parseLong(intervalTp);
+        final long LadurédelavidéoEnMinute = Long.parseLong(dureTotale);
+       
+        // nombre d'étape
+        final long nbEtapes = LadurédelavidéoEnMinute * 60 / intervalDeTempsEntreLesTp;
+        
+        // Téléporte le player à la 1er coordonée
+        ArrayList<Location> positions = this.getLocations(player);        
+        player.teleport(positions.get(0));
+        
+        // Les positions
+        Location initPos = positions.get(0);
+        Location finalPos = positions.get(1);
+
+        // calcule du decalage en X
+        final double offsetX = (finalPos.getX() - initPos.getX()) / nbEtapes;
+        final double offsetY = (finalPos.getY() - initPos.getY()) / nbEtapes;
+        final double offsetZ = (finalPos.getZ() - initPos.getZ()) / nbEtapes;
+        
+        final double offetYaw = ((finalPos.getYaw()) - initPos.getYaw()) / nbEtapes;
+        final double offsetPitch = (finalPos.getPitch() - initPos.getPitch()) / nbEtapes;
+        
+        player.sendMessage(initPos.toString());
+        player.sendMessage(finalPos.toString());
+
+        player.sendMessage("tu le fais en "  +nbEtapes + " étapes");
+        player.sendMessage("c'est à dire un décalage de "  +offsetX + " en X par étapes");
+        player.sendMessage("c'est à dire un décalage de "  +offsetY + " en Y par étapes");
+        player.sendMessage("c'est à dire un décalage de "  +offsetZ + " en Z par étapes");
+        player.sendMessage("c'est à dire un décalage de "  +offetYaw + " en Yaw par étapes");
+        player.sendMessage("c'est à dire un décalage de "  +offsetPitch + " en Pitch par étapes");
+        
+        
+        // Variable temporaire
+        Location oldLocation = player.getLocation();
+        final Location newLocation = oldLocation;
+        
+        final Player player2 = player;
+
+        t  = this.getTimer(player, true);
+         
+        t.schedule(new TimerTask() { 
+
+            long nbCurrentEtape = nbEtapes;
+
+            public void run() {
+                
+                nbCurrentEtape--;
+                System.out.println("Encore " + nbCurrentEtape +" étape sur " + nbEtapes);
+                
+                if(nbCurrentEtape <= 0)
+                {
+                    this.cancel();
+                }
+                
+                // mouvement sur X
+                newLocation.setX(newLocation.getX() + offsetX);
+                // mouvement sur Y
+                newLocation.setY(newLocation.getY() + offsetY);
+                // mouvement sur Z
+                newLocation.setZ(newLocation.getZ() + offsetZ);
+                
+                newLocation.setPitch((float) (newLocation.getPitch() + offsetPitch));
+                newLocation.setYaw((float) (newLocation.getYaw() + offetYaw));
+                
+                System.out.println("Yaw" +newLocation.getYaw());
+                System.out.println("Pitch " + newLocation.getPitch());
+                
+                // mouvement de tete vers la droite
+                // valeur possible: 
+                // -90(sud)
+                // -180 (est)
+                // 180 (ouest)
+                // 90(orienté nord)
+                //newLocation.setYaw((float) (newLocation.getYaw()+0.1));
+                 
+                // mouvement de tete vers le bas, valeur compris entre -90 (vers le haut du ciel)
+                // et +90 tu mattes tes pieds
+                //newLocation.setPitch((float) (newLocation.getPitch()+0.1));
+                player2.teleport(newLocation);
+                
+            }
+
+        }, 0, intervalDeTempsEntreLesTp * 1000);
+        
+        
+    }
+
+    public void cancelAnimate(Player player) {
+        // TODO Auto-generated method stub 
+        t  = this.getTimer(player, false);      
+        t.cancel();
+    }
+
+}
---
+++ src/onishinji/commands/AutoMoveCommand.java
@@ -0,0 +1,93 @@
+package onishinji.commands;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import onishinji.AutoMovePlugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class AutoMoveCommand implements CommandExecutor {
+
+    private AutoMovePlugin plugin;
+    private boolean isCommandStart;
+
+    public AutoMoveCommand(AutoMovePlugin cameraManPlugin, boolean b) {
+        // TODO Auto-generated constructor stub
+        plugin = cameraManPlugin;
+        
+        isCommandStart = b;
+    }
+
+    @Override 
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // TODO Auto-generated method stub
+        
+        if(!(sender instanceof Player))
+        {
+            return true;
+        }
+        
+        final Player player = (Player) sender;
+        
+        if(args.length < 1)
+        {
+            player.sendMessage(ChatColor.RED + "usage: /am start");
+            player.sendMessage(ChatColor.RED + "usage: /am stop");
+            player.sendMessage(ChatColor.RED + "usage: /am line intervaleDeTempsEntreLesTpEnSeconde laDuréDeLaVidoEnMinute");
+            
+            return false;
+        }
+        
+        String cmd = args[0];
+       
+        if(cmd.equals("start"))
+        {
+            plugin.registerFirstCoordinate(player);
+            return true;
+        }
+
+        if(cmd.equals("stop"))
+        {
+            plugin.registerSecondCoordinate(player);
+            return true;
+        }
+        
+
+        if(cmd.equals("debug"))
+        {
+            player.sendMessage(player.getLocation().toString());
+        }
+        
+
+        if(cmd.equals("cancel"))
+        {
+            plugin.cancelAnimate(player);
+        }
+
+
+        if(cmd.equals("line"))
+        {
+            if(args.length != 3)
+            {
+                player.sendMessage(ChatColor.RED + "Usage: /am line intervaleDeTempsEntreLesTpEnSeconde laDuréDeLaVidoEnMinute");
+                return false;
+            }
+            
+            String intervalTp = args[1];
+            String dureTotale = args[2];
+             
+            plugin.startLineAnimation(player, intervalTp, dureTotale);
+        }
+        
+       
+    
+        return false;
+    }
+
+}
